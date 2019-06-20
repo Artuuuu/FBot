2f4d0d810590e4ea279f834e12a83f062a2fdf25
Fajuu <akociszewski@protonmail.com>
2019-06-21 01:02:17 +0200

    Update FBot.php

--- FBot.php
+++ FBot.php
@@ -7,8 +7,8 @@ class FBot
 {
   #
   #
-  public $data;  // what you sent to Facebook   $fbot->message_text(json_encode($fbot->data));
-  public $response;  // what you got from Facebook   $fbot->message_text(json_encode($fbot->response));
+  public $data;  // what you sent to Facebook
+  public $response;  // what you got from Facebook
   private $config;
   private $client;
   #
@@ -19,30 +19,37 @@ class FBot
     
     /* Checks and Verify */
     $this->config = $config or die("Config not found!");
-    if(@$_REQUEST["hub_verify_token"] === $this->config["verifyToken"]) echo $_REQUEST["hub_challenge"];
+    if(@$_REQUEST["hub_verify_token"] === $this->config["verify_token"]) die($_REQUEST["hub_challenge"]);
     $this->input = json_decode(file_get_contents("php://input"), true) or die("You are not Facebook.");
     
     /* GuzzleHttp Client */
-    $client = new Client([
+    $this->client = new Client([
       "base_uri" => "https://graph.facebook.com/",
+      'exceptions' => false,
       "headers" => [
         "Content-Type" => "application/json",
         "User-Agent" => "FajuuFBot / beta"
-      ]
+      ],
     ]);
     
     /* Variables */
-    $this->client = $client;
     $this->persona = null;
-    $this->input = $this->input["entry"][0]["messaging"][0];
-    $this->sender = $this->input["sender"]["id"];
-    $this->recipient = $this->input["recipient"]["id"];
-    $this->message = $this->input["message"];
-    $this->postback = $this->input["postback"];
-    $this->text = $this->input["message"]["text"];
-    $this->qr_payload = $this->input["message"]["quick_reply"]["payload"];
-    $this->payload = $this->input["postback"]["payload"];
-    
+    $input = $this->input["entry"][0]["messaging"][0];
+    $this->sender = @$input["sender"]["id"];
+    $this->recipient = @$input["recipient"]["id"];
+
+    $this->received = json_decode(json_encode([
+      "text" => @$input["message"]["text"],
+      "payload" => @$input["postback"]["payload"] ?? @$input["message"]["quick_reply"]["payload"],
+      "message" => @$input["message"],
+      "postback" => @$input["postback"],
+      "quick_reply" => @$input["message"]["quick_reply"],
+      "is_echo" => (boolean)@$input["message"]["is_echo"],
+      "is_read" => (boolean)@$input["read"],
+      "is_delivery" => (boolean)@$input["delivery"],
+    ]), false);
+
+    $this->allow = (!@$this->received->message->is_echo and @$this->received->message) or $this->received->postback;
     #
   }
   #
@@ -52,7 +59,7 @@ class FBot
   public function lang($words)
   {
     #
-    $ldir = $this->config["localeFolder"] or die("localeFolder is not setted!");
+    $ldir = $this->config["locale_folder"] or die("locale_folder is not setted!");
     $lang = Yaml::parse(@file_get_contents("{$ldir}/{$this->user("locale")}.yml"))[$this->user("gender")] ?? Yaml::parse(@file_get_contents("{$ldir}/en_US.yml"))[$this->user("gender")];
     foreach($words as $word)
       $lang = $lang[$word];
@@ -69,20 +76,19 @@ class FBot
   public function user($data)
   {
     #
-    $this->curl->get("https://graph.facebook.com/{$this->sender}?fields={$data}&access_token={$this->config["accessToken"]}");
-    return $this->curl->response->{$data};
+    $this->response = $this->client->request("GET", "{$this->sender}?fields={$data}&access_token={$this->config["access_token"]}");
+    return json_decode($this->response->getBody(), true)[$data];
     #
   }
   #
   #
   #
   #
-  private function send($data)
+  private function request($data)
   {
     #
     $this->data = $this->persona ? array_merge($data, [ "persona_id" => $this->persona ] ) : $data;
-    $this->curl->post("https://graph.facebook.com/{$this->config["version"]}/me/messages?access_token={$this->config["accessToken"]}", $this->data);
-    $this->response = $this->curl->response;
+    if($this->allow) $this->response = $this->client->request("POST", "{$this->config["version"]}/me/messages?access_token={$this->config["access_token"]}", ["json" => $this->data]);
     #
   }
   #
@@ -91,7 +97,7 @@ class FBot
   public function say($text, $quick_replies = null)
   {
     #
-    $this->send(
+    $this->request(
       [
         "recipient" =>
         [
@@ -108,7 +114,7 @@ class FBot
   public function sender_action($type)
   {
     #
-    $this->send(
+    $this->request(
       [
         "recipient" =>
         [
@@ -125,7 +131,7 @@ class FBot
   public function template($type, $more)
   {
     #
-    $this->send(
+    $this->request(
       [
         "recipient" =>
         [
@@ -138,34 +144,6 @@ class FBot
             "type" => "template",
             "payload" => array_merge(["template_type" => "$type"], $more)
           ]
-        ]
-      ]
-    );
-    #
-  }
-  #
-  #
-  #
-  #
-  private function request($data)
-  {
-    #
-    $this->data = $data;
-    $this->curl->post("https://graph.facebook.com/{$this->config["version"]}/me/messenger_profile?access_token={$this->config["accessToken"]}", $this->data);
-    $this->response = $this->curl->response;
-    #
-  }
-  #
-  #
-  #
-  public function get_started($payload)
-  {
-    #
-    $this->request(
-      [
-        "get_started" =>
-        [
-          "payload" => $payload
         ]
       ]
     );
